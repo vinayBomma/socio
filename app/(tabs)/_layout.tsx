f18adbba12e4f34a1f329a88f7f7d7a46d909ffb
vinayBomma <vinaysb2000@gmail.com>
2024-07-04 10:18:42 +0530

    Added bottom tabs

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,14 +1,51 @@
-import { StyleSheet, Text, View } from "react-native";
 import React from "react";
+import { Tabs } from "expo-router";
+import { Feather } from "@expo/vector-icons";
 
 const TabsLayout = () => {
   return (
-    <View>
-      <Text>TabsLayout</Text>
-    </View>
+    <>
+      <Tabs screenOptions={{ tabBarShowLabel: false }}>
+        <Tabs.Screen
+          name="home"
+          options={{
+            headerShown: true,
+            title: "Home",
+            tabBarIcon: ({ color, focused }) => {
+              return <Feather name="home" size={24} color="black" />;
+            },
+          }}
+        />
+        <Tabs.Screen
+          name="stats"
+          options={{
+            title: "Statistics",
+            tabBarIcon: ({ color, focused }) => {
+              return <Feather name="bar-chart-2" size={24} color="black" />;
+            },
+          }}
+        />
+        <Tabs.Screen
+          name="chat"
+          options={{
+            title: "Chat",
+            tabBarIcon: ({ color, focused }) => {
+              return <Feather name="message-circle" size={24} color="black" />;
+            },
+          }}
+        />
+        <Tabs.Screen
+          name="profile"
+          options={{
+            title: "Profile",
+            tabBarIcon: ({ color, focused }) => {
+              return <Feather name="user" size={24} color="black" />;
+            },
+          }}
+        />
+      </Tabs>
+    </>
   );
 };
 
 export default TabsLayout;
-
-const styles = StyleSheet.create({});
