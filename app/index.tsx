32c15ce1683dc106923d16bfa2ce04d5a11af2c5
vinayBomma <vinaysb2000@gmail.com>
2024-07-10 09:56:23 +0530

    Refactored notifications

--- app/index.tsx
+++ app/index.tsx
@@ -1,14 +1,49 @@
-import { Link } from "expo-router";
+import { Link, router } from "expo-router";
 import { Text, TouchableOpacity, View } from "react-native";
 import { StatusBar } from "expo-status-bar";
 import GoogleAuth from "@/components/GoogleAuth";
-import { supabase } from "@/lib/supabase";
+import getGPTResponse from "@/lib/gpt.ts";
+import supabase from "@/lib/supabase.ts";
+import * as Notifications from "expo-notifications";
+import { useEffect } from "react";
+
+Notifications.setNotificationHandler({
+  handleNotification: async () => ({
+    shouldShowAlert: true,
+    shouldPlaySound: false,
+    shouldSetBadge: false,
+  }),
+});
 
 export default function App() {
+  useEffect(() => {});
   const handleGuestLogin = async () => {
     const { data, error } = await supabase.auth.signInAnonymously();
-    console.log(data, error);
+    console.log("data", data);
+    if (data) {
+      router.push("/home");
+    }
   };
+
+  const handleNotification = async () => {
+    console.log("handleNotification");
+    await schedulePushNotification();
+  };
+
+  async function schedulePushNotification() {
+    const data = await getGPTResponse(
+      "User has consistently being exercising since 5 days"
+    );
+    console.log("data", data);
+    await Notifications.scheduleNotificationAsync({
+      content: {
+        title: "You've got mail! 📬",
+        body: data,
+      },
+      trigger: null,
+    });
+  }
+
   return (
     <View className="flex-1 items-center justify-center bg-slate-400">
       <Text className="text-3xl">Social Habits</Text>
@@ -23,6 +58,9 @@ export default function App() {
       <Link className="text-xl" href="/home">
         Open Tabs
       </Link>
+      <TouchableOpacity onPress={handleNotification}>
+        <Text className="text-xl m-3">Send Notifications</Text>
+      </TouchableOpacity>
     </View>
   );
 }
